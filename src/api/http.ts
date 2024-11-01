aebb35b070fe5b3c4fbb3487133f1d1f440acdf4
thai le <thailnp1303@gmail.com>
2024-11-01 11:02:25 +0700

    update

--- src/api/http.ts
+++ src/api/http.ts
@@ -3,7 +3,10 @@ import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios'
 export class Http {
   instance: AxiosInstance
 
-  constructor(baseURL: string = 'http://localhost:3000/api/v1', headers: Record<string, string> = {}) {
+  constructor(
+    baseURL: string = 'https://booking-event-a720ee108ec8.herokuapp.com/api/v1',
+    headers: Record<string, string> = {}
+  ) {
     let authHeader = headers['Authorization'] || localStorage.getItem('Authorization') || null
 
     if (authHeader && !authHeader.startsWith('Bearer ')) {
